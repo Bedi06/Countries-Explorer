683be03ebd42a27ff4a33a69034bdf829df7b0d4
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-08-18 15:46:31 +0100

    tiny fix

--- src/CoutriesCards.js
+++ src/CoutriesCards.js
@@ -5,7 +5,7 @@ function CountriesCards ({allCountries,DarkMode}){
   return (
     <div className="grid-container">
     {allCountries.map((country,index)=>(
-      <div key={index} className="card"style={{width: "18rem" ,paddingLeft:"2rem"}}>
+      <div key={index} className={`card ${DarkMode ? "dark" : ""}`} style={{width: "18rem" ,paddingLeft:"2rem"}}>
          <img src={country.flag} alt="countries-flag" />
          
          <h3 >{country.name}</h3>
