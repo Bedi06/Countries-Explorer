36792315d23bf76d929ad8cc555bb2a42f1c4f40
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2024-02-07 16:17:10 +0000

    fixed some styling to be more responsive

--- src/Header.js
+++ src/Header.js
@@ -3,22 +3,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function Header({ darkMode, handleDarkMode }) {
   return (
     <header className={`header-container ${darkMode ? "dark" : ""}`}>
-      <div className="header-elem">
-        <h1 className="header-h1">Where in the world?</h1>
-        <div className="light-dark">
-          <input
-            type="checkbox"
-            className="checkbox"
-            id="checkbox"
-            checked={darkMode}
-            onChange={handleDarkMode}
-          />
-          <label htmlFor="checkbox" className="checkbox-label">
-            <FontAwesomeIcon icon="sun" />
-            <FontAwesomeIcon icon="moon" />
-            <span className="ball"></span>
-          </label>
-        </div>
+      <h1 className="header-h1">Where in the world?</h1>
+      <div className="light-dark">
+        <input
+          type="checkbox"
+          className="checkbox"
+          id="checkbox"
+          checked={darkMode}
+          onChange={handleDarkMode}
+        />
+        <label htmlFor="checkbox" className="checkbox-label">
+          <FontAwesomeIcon icon="sun" />
+          <FontAwesomeIcon icon="moon" />
+          <span className="ball"></span>
+        </label>
       </div>
     </header>
   );
